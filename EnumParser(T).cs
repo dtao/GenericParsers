4119474492526cd5ed51fe42f9be641311b52e61
Dan Tao <daniel.tao@gmail.com>
2013-05-30 16:33:28 -0700

    fixed EnumParser inheritance tree

--- EnumParser(T).cs
+++ EnumParser(T).cs
@@ -2,14 +2,14 @@ using System;
 
 namespace GenericParsers
 {
-    public class EnumParser<T> : IParser<T> where T : struct
+    public class EnumParser<T> : Parser<T> where T : struct
     {
-        public T Parse(string input)
+        public override T Parse(string input)
         {
             return (T)Enum.Parse(typeof(T), input);
         }
 
-        public bool TryParse(string input, out T result)
+        public override bool TryParse(string input, out T result)
         {
             return Enum.TryParse(input, out result);
         }
